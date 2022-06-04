e15f1dc42e00954ee3dd238d0756eaef4d1fc21c
BillyMin <alstmdrl7886@gmail.com>
2022-06-04 16:04:52 +0900

    add

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -12,7 +12,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Colume(db.Boolean(), default=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
     
     def __init__(self, email):
         self.email = email
