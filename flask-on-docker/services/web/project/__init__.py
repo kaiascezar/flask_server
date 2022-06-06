e461ef33aee5bdc25cc5902e8c0136106049efb4
BillyMin <alstmdrl7886@gmail.com>
2022-06-06 19:37:21 +0900

    password String length change

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -11,9 +11,9 @@ class User(db.Model):
     __tablename__ = "users"
     
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    decryptkey = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    decryptkey = db.Column(db.String(100), nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
     
     def __init__(self, user_id):
