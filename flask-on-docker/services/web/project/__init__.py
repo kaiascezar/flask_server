80bc7dc6dfabf246194d7316b71e39dd7662050e
BillyMin <alstmdrl7886@gmail.com>
2022-06-07 10:20:47 +0900

    login 구현

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
+import json
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+
 
 
 app = Flask(__name__)
@@ -14,14 +18,70 @@ class User(db.Model):
     user_id = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.Unicode(100), nullable=False)
     decryptkey = db.Column(db.String(100), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+    
     
     def __init__(self, user_id, password, decryptkey):
         self.user_id = user_id
-        self.password = password
+        self.set_password(password)
         self.decryptkey = decryptkey
-
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 @app.route('/')
 def hello_world():
     return jsonify(hello="world")
+
+@app.route("sign-up", methods=['POST'])
+def sign_up():
+    new_user = request.json
+    new_user['password'] bcrypt.hashpw(
+        new_user['password'].encode('UTF-8'),
+        bcrypt.salt('secret')
+    )
+    
+    net_user_id = app.database.execute(text("""
+        INSERT INTO users(
+            user_id,
+            hashed_password,
+            decryptkey
+        ) VALUES (
+            :user_id,
+            hashed_password,
+            decryptkey
+        )    
+    """), new_user).lastrowid
+    new_user_info = get_user(new_user_id)
+    
+    return jsonify(new_user_info)
+
+@app.route("/login", methods=['POST'])
+def login():
+    credential = request.json
+    email = credential['user_id']
+    password = credential['password']
+    
+    row = database.execute(text(""".
+        SELECT
+            id,
+            hashed_password
+        FROM users
+        WHERE user_id = :user_id
+    """), {'user_id' : user_id}).fetchone()
+    
+    if row and bcrypt.checkpw(password.encode('UTF-8'), row['hashed_password'].encode('UTF-8')):
+        user_id = row['user_id']
+        payload = {
+            'user_id' : user_id,
+            'exp' : datatime.utcnow() + timedelta(seconds = 60 * 60 * 24)
+        }
+        token = jwt.encode(payload, app.config['JWT_SECRET_KEY'],
+        'HS256')
+        
+        return jsonify({
+            'access_token' : token.decode('UTF-8')
+        })
+    else:
+        return '', 401
