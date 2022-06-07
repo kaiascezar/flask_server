36c4b73fe16432afc6baa3538bba34ccdfd7730c
BillyMin <alstmdrl7886@gmail.com>
2022-06-07 10:40:36 +0900

    change password size

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -17,7 +17,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.Unicode(100), nullable=False)
+    password = db.Column(db.Unicode(256), nullable=False)
     decryptkey = db.Column(db.String(100), nullable=False)
     
     
