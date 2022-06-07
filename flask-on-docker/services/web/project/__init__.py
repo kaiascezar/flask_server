0de6a08ef647dfdb843e49b690bd0b2a2d23d0f3
BillyMin <alstmdrl7886@gmail.com>
2022-06-07 15:46:34 +0900

    asdf

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -21,7 +21,7 @@ class User(db.Model):
     password = db.Column(db.Unicode(256), nullable=False)
     
     
-    def __init__(self, user_id, password, decryptkey):
+    def __init__(self, user_id, password):
         self.user_id = user_id
         self.password = password
     
