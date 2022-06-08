6085ae9ee7a2589076fe53dc49c81a3dcebdcb8c
BillyMin <alstmdrl7886@gmail.com>
2022-06-08 16:59:31 +0900

    add

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, TEXT, INTEGER
@@ -34,15 +34,19 @@ class Check(Resource):
 
 api.add_resource(Check, '/fruit')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
-    name = request.form['name']
-    password = request.form['password']
-    db.execute("select id, password from users where id='{}' and password = '{}';".format(name, password))
-    result = db.fetchall()
-    if not result:
-        return "비밀번호를 확인하세요", 400
-    elif result[0] == name and result[1] == password:
-        return "Access Token", 200
-    
-    
+    if request.method == 'GET':
+        return '홈페이지'
+    else:
+        name = request.form['name']
+        password = request.form['password']
+        try:
+            data = User.query.filter_by(username=name, password=password).first()
+            if data is not None:
+                session['logged_in'] = True
+                return '로그인 완료'
+            else:
+                return '로그인 안됨'
+        except:
+            return '로그인 안됨'
