c210c76d1fa091873c7bbf583e98d3afc111fab6
BillyMin <alstmdrl7886@gmail.com>
2022-06-09 06:55:40 +0900

    add

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -12,7 +12,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True, nullable=False)
-    password = db.Column(db.String(250),  nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
     def __init__(self, name, password):
         self.name = name
