df701793aa108a111fdfbba741ff99afd4adb59a
BillyMin <alstmdrl7886@gmail.com>
2022-06-08 09:30:16 +0900

    add

--- flask-on-docker/services/web/project/config.py
+++ flask-on-docker/services/web/project/config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://GTN_Admin:GTNAdmin!123@db:5432/GTN_User")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://{DB['GTN_Admin']}:{DB['GTNAdmin!123']}@{DB['db:5432']}/{DB['GTN_User']}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
