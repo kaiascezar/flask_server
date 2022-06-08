df8407822320e34a5169129d0231a8e79acc0045
BillyMin <alstmdrl7886@gmail.com>
2022-06-08 22:48:42 +0900

    add

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -13,9 +13,10 @@ def page_not_found(error):
 
 class User(db.Model):
 	__tablename__ : "users"
+
 	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True)
-	password = db.Column(db.String(80))
+	username = db.Column(db.String(80), unique=True, nullable=False)
+	password = db.Column(db.String(80), nullable=False)
 
 	def __init__(self, username, password):
 		self.username = username
