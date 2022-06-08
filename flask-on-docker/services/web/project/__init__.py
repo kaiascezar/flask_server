212d152d4ec643fa982f42a4dfb1cc67afc98cbb
BillyMin <alstmdrl7886@gmail.com>
2022-06-08 10:17:31 +0900

    add

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, session, current_app, g
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, TEXT, INTEGER
+from sqlalchemy import text
 
 
 
@@ -21,6 +21,19 @@ class User(db.Model):
         self.userid = userid
         self.password = password
 
+def get_user_password(userid):
+    row = db.execute(text("""
+        select
+            userid,
+            password
+        from users
+        """)), {'userid' : userid}.fetchone()
+
+    return {
+        'userid' : row['userid'],
+        'password' : row['password']
+    } if row else None
+
 
 @app.route("/")
 def hello_world():
@@ -29,11 +42,17 @@ def hello_world():
 @app.route("/login", methods=['POST'])
 def login():
     if request.method == 'POST':
-        users = User.query.all()
-        results = [
-            {
-                "id": user.userid,
-                "password": user.password
-            } for user in users]
+        credential = request.json
+        userid = credential['userid']
+        password = credential['password']
+        user_credential = get_user_password(userid)
+        
+        if userid == user_credential['userid'] and password == user_credential['password']:
+            'userid' = user_credential['id']
+            
+            return jsonify({'access Token'})
+            
+        
+        results = db.fetchone
         
         return {results}
