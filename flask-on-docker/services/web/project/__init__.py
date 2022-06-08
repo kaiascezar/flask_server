7a501cb58c50b270aa15459d976de31b9c1886f8
BillyMin <alstmdrl7886@gmail.com>
2022-06-09 07:03:42 +0900

    add

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -13,13 +13,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
-    
+
     def __init__(self, name, password):
         self.name = name
         self.password = password
 
 
 @app.route("/login")
-def hello_world():
+def login():
     user = User.query.first()
     return jsonify('hello' + user.name + user.password)
