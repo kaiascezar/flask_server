6fe5cfc88c3574b7c798ffd25b958ae3b43ba2cc
BillyMin <alstmdrl7886@gmail.com>
2022-06-08 15:49:44 +0900

    add

--- flask-on-docker/services/web/manage.py
+++ flask-on-docker/services/web/manage.py
@@ -1,5 +1,5 @@
 from flask.cli import FlaskGroup
-from project import app, db
+from project import app, db, Check
 
 
 cli = FlaskGroup(app)
@@ -14,7 +14,7 @@ def create_db():
 
 @cli.command("seed_db")
 def seed_db():
-#    db.session.add(User(userid="BillyMin", password='test1234!'))
+    db.session.add(Check(name="BillyMin", color='test1234!'))
     db.session.commit()
     
 
