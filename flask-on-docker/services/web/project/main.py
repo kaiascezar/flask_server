5d2bb911fb8f9187bbd0c129060506f5c9399520
BillyMin <alstmdrl7886@gmail.com>
2022-06-10 19:16:32 +0900

    add

--- flask-on-docker/services/web/project/main.py
+++ flask-on-docker/services/web/project/main.py
@@ -50,7 +50,7 @@ def login():
     pw = request.form['password']
     
     pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
-    result = db.session.query.filter(User(id, pw_hash)).all
+    result = db.query.filter(User(id, pw_hash)).all
     
     return jsonify({
         'id' : id,
