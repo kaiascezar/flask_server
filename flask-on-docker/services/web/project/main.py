1f1bb7851e118dd5ba1541602cdb1e9f77a968d0
BillyMin <alstmdrl7886@gmail.com>
2022-06-10 13:37:08 +0900

    opencv run test

--- flask-on-docker/services/web/project/main.py
+++ flask-on-docker/services/web/project/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-# from datetime import datetime, timedelta
-from project.detectionwork import GtnOcr
+from datetime import datetime, timedelta
+from project.detectionwork import GtnOcr, PreProcessing
 
 
 app = Flask(__name__)
