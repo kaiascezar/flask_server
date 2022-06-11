e9555e1f94505b4ab5efb11b978fcd146e459546
BillyMin <alstmdrl7886@gmail.com>
2022-06-11 10:34:04 +0900

    modify login def

--- flask-on-docker/services/web/project/main.py
+++ flask-on-docker/services/web/project/main.py
@@ -91,6 +91,7 @@ def login():
             })
     else:
         return jsonify({'result': 'fail', 'msg':'아이디/비밀번호가 일치하지 않습니다.'})
+
 #    if result is not None:
 #        payload = {
 #            'id' : id,
