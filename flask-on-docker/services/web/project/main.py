c23656ecc92dc9c979dac54d8bf7d96a0fe93d57
BillyMin <alstmdrl7886@gmail.com>
2022-06-13 17:18:58 +0900

    login api 입력받은 pw 해시화

--- flask-on-docker/services/web/project/main.py
+++ flask-on-docker/services/web/project/main.py
@@ -96,8 +96,12 @@ def login():
     pw = auth['pw']
     user_auth = get_user_id_password(id)
     
+    pw_hash = bcrypt.hashpw(pw.encode('UTF-8'),
+                            bcrypt.gensalt()
+                            )
+    
          
-    if user_auth and bcrypt.checkpw(pw.encode('UTF-8'), user_auth['pw'].encode('UTF-8')):
+    if user_auth and bcrypt.checkpw(pw_hash, user_auth['pw'].encode('UTF-8')):
         user_id = user_auth['index']
         payload = {
             'index' : user_id,
