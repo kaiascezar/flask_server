0ba7c8fe8f9e0994a54bbae4f1f057b19a9a12b0
BillyMin <alstmdrl7886@gmail.com>
2022-06-13 17:07:10 +0900

    db_pw 함수 추가 db_pw를 decode

--- flask-on-docker/services/web/project/main.py
+++ flask-on-docker/services/web/project/main.py
@@ -96,7 +96,9 @@ def login():
     pw = auth['pw']
     user_auth = get_user_id_password(id)
     
-    pwhash_decode = user_auth['pw'].decode('UTF-8')
+    db_pw = user_auth['pw']
+    
+    pwhash_decode = db_pw.decode('UTF-8')
          
     if user_auth and bcrypt.checkpw(pw.encode('UTF-8'), pwhash_decode.encode('UTF-8')):
         user_id = user_auth['index']
